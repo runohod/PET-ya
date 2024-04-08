910f6d798a5db379c4e640c1c40ab80f13c38688
runohod <andrei.rumynthev1728@yandex.ru>
2024-04-08 16:35:52 +0300

    1

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const inputElement = document.ElementById('title')
-const inputElement = document.ElementById('title')
-const inputElement = document.ElementById('title')
+const inputElement = document.ElementById('create')
+const inputElement = document.ElementById('list')
+
+createBtn.onclick = function () {
+        listElement.innerHTML = `<li
+        class="list-group-item d-flex justify-content-between align-items-center"
+    >
+        <span>${inputElement.value}</span>
+        <span>
+        <span class="btn btn-small btn-success">&check;</span>
+        <span class="btn btn-small btn-danger">&times;</span>
+        </span>
+    </li>`
+}
