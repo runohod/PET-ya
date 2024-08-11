3c5edc404ce39b933f7fee5f8bae27077736e44a
runohod <andrei.rumynthev1728@yandex.ru>
2024-08-11 19:23:08 +0300

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', function (event) {
+//     console.log(event)
+// });
+
+// const button = document.querySelector('.button');
+
+// button.addEventListener('click', function (evt) {
+//   // в переменной eventTarget окажется элемент
+//   // button, на который мы кликнули
+
+//     const eventTarget = evt.target;
+//     eventTarget.setAttribute('disabled', true);
+// });
+
+
+// const password = prompt('Введите пароль:', '');
+
+// for (let i = 0; i <= password.length; i = i + 1) {
+//     if (password.charAt(i) === '?') {
+//     console.log('"?" есть в пароле на позиции ' + (i + 1));
+// }
+// }
+
+// position.foreach(function(item){
+//     console.log(item);
+// });
+
+// const counterIndexed = counter.map(function(person, index){
+//     return `${index + 1}. ${person}`; 
+// }) 
+
+// console.log(counterIndexed);
+
+// const ivansIndexed = ivans.map(function(item, index, array) {
+//     const currentIndex = `(${(index + 1)} из ${array.length})`;
+//     return `${item} ${currentIndex}`;
+//   });
+
+
+
