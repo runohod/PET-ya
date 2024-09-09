9de4e015cb216c6e3b7e45617f9af882fef8a895
runohod <andrei.rumynthev1728@yandex.ru>
2024-09-09 21:58:06 +0300

    init

--- index.tsx
+++ index.tsx
@@ -1,12 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+const flowers: string = 'Розы';
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+console.log(flowers);
