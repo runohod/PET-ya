d0b8c3ce94fbd9149d10015c47bf579b13a1c78d
runohod <andrei.rumynthev1728@yandex.ru>
2024-09-13 15:45:43 +0300

    init

--- index.tsx
+++ index.tsx
@@ -1,75 +1,15 @@
-const flowers: string = 'Розы';
+class User () {
+    _defoltroot = false;
 
-console.log(flowers);
-
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { getToken, getUser } from '../../api/user';
-import { User } from '../../types';
-
-export const loginUserThunk = createAsyncThunk(
-    'users/loginUser',
-    ({login, password}: {login: string; password: string}) =>
-        getToken({login, password})
-            .then(token => {
-                localStorage.setItem('token', token);
-                return token;
-            }),
-)
-
-export const getUserThunk = createAsyncThunk(
-    'users/getUser',
-    ({token}: {token: string}) =>
-        getUser({token}),
-)
-
-export interface UserState {
-    isInit: boolean;
-    isLoading: boolean;
-    user: User | null;
-    error: string | null;
-}
-
-const initialState: UserState = {
-    isInit: false,
-    isLoading: false,
-    user: null,
-    error: null,
-}
-
-export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        init: (state) => {
-            state.isInit = true;
-        },
-    },
-    extraReducers: (builder) => {
-        builder.addCase(loginUserThunk.pending, (state) => {
-            state.isLoading = true;
-        });
-        builder.addCase(loginUserThunk.rejected, (state) => {
-            state.isLoading = false;
-        });
-        builder.addCase(loginUserThunk.fulfilled, (state) => {
-            state.isLoading = false;
-        });
-
-        builder.addCase(getUserThunk.pending, (state) => {
-            state.isLoading = true;
-        });
-        builder.addCase(getUserThunk.rejected, (state) => {
-            state.isInit = true;
-            state.isLoading = false;
-        });
-        builder.addCase(getUserThunk.fulfilled, (state, {payload}) => {
-            state.isInit = true;
-            state.isLoading = false;
-            state.user = payload;
-        });
+    constructor(Login) {
+        this.login = login; 
     }
-});
 
-export const {init} = userSlice.actions;
+    giveExtraRoot () {
+        this._defoltroot = true;
+    }
+} 
 
-export default userSlice.reducer
+auth (value: string) {
+    return this.password === value;
+} 
